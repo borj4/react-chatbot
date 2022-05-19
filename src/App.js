bf112e106823f9338ba8c3ab102d860d1ec854db
borj4 <river4car@gmail.com>
2022-05-19 19:17:22 +0200

    update: first version

--- src/App.js
+++ src/App.js
@@ -1,41 +1,44 @@
 import React, { useEffect, useState } from 'react';
-import Respose from './components/Response/Response'
+import axios from 'axios'
+import spinner from './assets/loading.gif';
 import './App.css';
 
 const App = () => {
 
-  const [response, setResponse] = useState('');
-  const [write, setWrite] = useState('')
+  const [value, setValue] = useState('');
+  const [botResponse, setBotResponse] = useState('Hello, how can I help you?');
+  const [userResponse, setUserResponse] = useState('')
+  const [loading, setLoading] = useState(false)
 
 
   const handleButton = async (e) => {
     e.preventDefault();
-    await setTimeout(setResponse(""),500);
-    setResponse(e.target.user.value)
+    setUserResponse(e.target.user.value);
+    setValue('')
   }
 
-  // const renderResponse = (input) => {
-  //   return <h5 className='response'>{input}</h5>
-  // }
+  useEffect(()=>{
+    setLoading(true)
+    try{
+      axios.post('http://almesa93.pythonanywhere.com/',{variable: userResponse})
+      .then(response=>JSON.parse(response).text.replace('<br><br>',''))
+      .then(response=>console.log("la respuesta es "+response))
+      .then(response=>setBotResponse(response))
+    }
+    catch (error) { console.log(error) }
+    finally { setLoading(false) }
+    
+  },[userResponse])
 
-  // useEffect(()=>{
-  //   setWrite(response)
-  //   renderResponse(write)
-  // },[response])
-
-
-  /////// cleaning
-  // JSON.parse(<RESPONSE>).text.replace('<br><br>','')
-  ////////////////
 
   return (
     <div className='App'>
     <div className='containerResponse'>
-      {response ? <h5 className='response'>{response}</h5> : ""}
+      {loading ? <img className="spinner" src={spinner} alt="loading..." /> : <h5 className='response'>{botResponse}</h5>}
     </div>
 
     <form onSubmit={handleButton}  >
-      <input name="user"/>
+      <input value={value} onChange={e=>setValue(e.target.value)} name="user"/>
       <button type='submit'>Enviar</button>
     </form>
     </div>
